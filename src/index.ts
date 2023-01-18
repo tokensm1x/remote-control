8533320bb7392d549d5dbae3321a71b4358d3dd1
tokensm1x <tokensmix@gmail.com>
2023-01-18 16:39:04 +0300

    fix: modules

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { httpServer } from "./http-server";
 import "dotenv/config";
-import { wss } from "./wss-server";
+import { wss } from "./ws-server";
 
 const APP_PORT: number = +process.env.APP_PORT || 3000;
 
